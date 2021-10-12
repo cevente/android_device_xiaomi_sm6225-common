660b6196f9bf623c01ca0a2abd2e45587afdbca0
Danny Lin <danny@kdrag0n.dev>
2021-10-11 18:40:13 -0700

    sapphire: Optimize native executables for Cortex-A73 CPU
    Allow the compiler to take advantage of ARMv8a extensions to improve
    performance in native executables, not just Java code compiled by ART.
    
    Change-Id: I0e28dee8fb0e587dc0ceb742daa083d9cd16138d
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,7 +40,7 @@ BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a73
 
 # Audio
 $(call soong_config_set, android_hardware_audio, run_64bit, true)
