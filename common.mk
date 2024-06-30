f7e50c7d4402666e1ed93a21ab67896ad2b2913e
Pranav Vashi <neobuddy89@gmail.com>
2024-07-01 00:15:41 +0530

    sapphire: Add missing stagefright libraries
    06-30 23:37:15.059  1475  1475 E vndksupport: Could not load libstagefrightdolby.so from default namespace: dlopen failed: library "libstagefright_softomx.so" not found: needed by /vendor/lib/libstagefrightdolby.so in namespace (default).
    06-30 23:37:15.060  1475  1475 E vndksupport: Could not load libstagefright_softomx_plugin.so from default namespace: dlopen failed: library "libstagefright_softomx_plugin.so" not found.
    
    Change-Id: Ic073c46f938e4875ca45b41dc32bc69fce734c87
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -409,7 +409,18 @@ endif
 # OMX
 PRODUCT_PACKAGES += \
     libmm-omxcore \
-    libOmxCore
+    libOmxCore \
+    libopus.vendor \
+    libstagefright_amrnb_common.vendor \
+    libstagefright_enc_common.vendor \
+    libstagefright_softomx.vendor \
+    libstagefright_softomx_plugin.vendor \
+    libstagefrighthw \
+	libstagefright_omx.vendor \
+    libstagefright_softomx.vendor \
+    libstagefright_softomx_plugin.vendor \
+    libvorbisidec.vendor \
+    libvpx.vendor
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
