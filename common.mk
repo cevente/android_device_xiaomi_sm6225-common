81197c8aacad42c7f66fc7ed8d601e0a64ce6d99
Liekoo <veyeka1@gmail.com>
2025-07-10 12:03:46 +0800

    sapphire: build IR AIDL from source

--- common.mk
+++ common.mk
@@ -287,7 +287,7 @@ PRODUCT_PACKAGES += \
 # IR
 ifneq ($(TARGET_IS_TABLET),true)
 PRODUCT_PACKAGES += \
-    android.hardware.ir-V1-ndk.vendor
+    android.hardware.ir-service.example
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
