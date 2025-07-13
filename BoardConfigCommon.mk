cf86ae1caf599a34f17ace114491121d8870131a
Ali Hasan <ahb7671@gmail.com>
2025-07-13 21:07:32 +0800

    sapphire: fix and move NFC to different fragment and enable only on Sapphiren {n7n, n7ns}

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -222,6 +222,9 @@ DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/configs/vintf/manifest_phone.xml
 endif
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+ODM_MANIFEST_SKUS += n7n n7n_ss
+ODM_MANIFEST_N7N_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
+ODM_MANIFEST_N7NS_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
