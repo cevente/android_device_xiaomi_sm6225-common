062fa19360fc599f129268fce982ce3161bbff2f
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-08-21 12:12:55 +0800

    sapphire: Import Parts from fuxi
    fuxi: Enable touchscreen feature based on settings
    
    Single tap gesture and always on udfps rely on mode 16 to be enabled, so
    setup a listener for those settings and set the mode enabled if one of
    both is on
    
    fuxi: Start parts services as early as possible on bootup
    
    BOOT_COMPLETED intent is broadcast only after the device is unlocked and
    user data (encrypted storage) is available. Use LOCKED_BOOT_COMPLETED
    intent for services that don't require accessing data.

--- common.mk
+++ common.mk
@@ -309,6 +309,7 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.target.rc \
     init.xiaomi.rc \
+    init.mi_touch.rc \
     ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
@@ -446,6 +447,10 @@ endif
 # Overlays Lineage
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
+
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
