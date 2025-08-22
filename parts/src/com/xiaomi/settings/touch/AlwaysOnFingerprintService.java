3127d21cfe42ec36d204a9c3ea37a9bd1ee1c517
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-08-22 15:14:33 +0800

    sapphire: Don't update TOUCH_FOD_ENABLE when FOD is pressed
    * almost same reason as f03719d54a26dc465a5bddd89b2ee4ed08e9bc2e

--- parts/src/com/xiaomi/settings/touch/AlwaysOnFingerprintService.java
+++ parts/src/com/xiaomi/settings/touch/AlwaysOnFingerprintService.java
@@ -23,6 +23,8 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 
+import com.xiaomi.settings.utils.FileUtils;
+
 public class AlwaysOnFingerprintService extends Service {
 
     private static final String TAG = "XiaomiPartsAlwaysOnFingerprintService";
@@ -30,6 +32,8 @@ public class AlwaysOnFingerprintService extends Service {
 
     private static final String SECURE_KEY_TAP = "doze_tap_gesture";
     private static final String SECURE_KEY_UDFPS = "screen_off_udfps_enabled";
+
+    private static final String FOD_PRESS_STATUS_PATH = "/sys/class/touch/touch_dev/fod_press_status";
 
     private boolean mIsAofEnabled;
 
@@ -79,8 +83,12 @@ public class AlwaysOnFingerprintService extends Service {
             if (DEBUG) Log.d(TAG, "onReceive: " + intent.getAction());
             int displayState = getDisplay().getState();
             boolean displayStateAof = displayState != Display.STATE_ON && mIsAofEnabled;
-            TfWrapper.setTouchFeature(
-                    new TfWrapper.TfParams(/*TOUCH_FOD_ENABLE*/ 10, displayStateAof ? 1 : 0));
+            if (FileUtils.readLineInt(FOD_PRESS_STATUS_PATH) == 1) {
+                if (DEBUG) Log.d(TAG, "onReceive: FOD active, dont update TOUCH_FOD_ENABLE!");
+            } else {
+                TfWrapper.setTouchFeature(
+                        new TfWrapper.TfParams(/*TOUCH_FOD_ENABLE*/ 10, displayStateAof ? 1 : 0));
+            }
             TfWrapper.setTouchFeature(
                     new TfWrapper.TfParams(/*TOUCH_FODICON_ENABLE*/ 16, displayStateAof ? 1 : 0));
         }
