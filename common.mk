e1d0495ad01034633c16c25d666509863c983e44
LuK1337 <priv.luk@gmail.com>
2025-09-03 16:43:33 +0800

    sapphire: Build gralloc.qcom
    Camera HAL wants it and crashes if it loads default gralloc
    implementation instead.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8059

--- common.mk
+++ common.mk
@@ -172,6 +172,7 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libgralloc.qti \
+    gralloc.qcom \
     libqdMetaData \
     libqdMetaData.system \
     libsdmcore \
