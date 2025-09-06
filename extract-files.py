7d0278aca92ff5bee0a6af03a26774df6e0ec1d3
Liekoo <veyeka1@gmail.com>
2025-09-06 20:33:01 +0800

    sm6225-common: fix elf checks

--- extract-files.py
+++ extract-files.py
@@ -16,6 +16,37 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
+namespace_imports = [
+    'device/xiaomi/sm6225-common',
+    'hardware/qcom-caf/sm6225',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+]
+
+libs_add_vendor_suffix = (
+    'vendor.qti.hardware.qccsyshal@1.0',
+    'vendor.qti.hardware.qccsyshal@1.1',
+    'vendor.qti.hardware.qccsyshal@1.2',
+    'vendor.qti.hardware.qccvndhal@1.0',
+    'vendor.qti.hardware.sigma_miracast@1.0',
+    'vendor.qti.hardware.wifidisplaysession@1.0',
+    'vendor.qti.imsrtpservice@3.0',
+    'vendor.qti.imsrtpservice@3.1',
+    'vendor.qti.diaghal@1.0',
+    'com.qualcomm.qti.dpm.api@1.0',
+)
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    libs_add_vendor_suffix: lib_fixup_vendor_suffix,
+    libs_remove: lib_fixup_remove,
+}
+
 def blob_fixup_test_flag(
     ctx: BlobFixupCtx,
     file: File,
@@ -58,6 +89,8 @@ module = ExtractUtilsModule(
     'sm6225-common',
     'xiaomi',
     blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
